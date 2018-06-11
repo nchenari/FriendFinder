081e640872fb44fc9a003ecc519bc34fde8adf6a
nchenari <nchenari@iMac.local>
2018-06-11 17:51:16 -0400

    Started README.md

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,12 +6,12 @@
 // ===============================================================================
 
 var friendArray = [
-    {
-        "name": "Nima",
-        "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Kermit_the_Frog.jpg/220px-Kermit_the_Frog.jpg",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-    }
-]
+    // {
+    //     "name": "Nima",
+    //     "photo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/62/Kermit_the_Frog.jpg/220px-Kermit_the_Frog.jpg",
+    //     "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    // }
+];
 
 // export array for use in other files using require
 module.exports = friendArray;
