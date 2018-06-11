49af6a7d0642a048d93e575c8183da542632a578
nchenari <nchenari@iMac.local>
2018-06-11 02:08:09 -0400

    added form questions

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     // API HTML GET Requests
     // Below code handles when users visit a page
     // In each of the below cases the user is shown an HTML page of content
-
+    // This will be used to display a JSON of all possible friends
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
@@ -19,9 +19,21 @@ module.exports = function(app) {
     // API HTML POST Requests
     // Below code handles when a user submits a form and thus submits data to the server
     // JSON is pushed to appropriate JavaScript array
-
-    api.post("/api/friends", function(req, res) {
+    // Used to handle incoming survey results. This route will also be used to handle 
+    // ...compatibility logic. 
+    app.post("/api/friends", function(req, res) {
         // req.body is available since we're using the body-parser middleware
         friends.push(req.body);
+
+        // handle friend compatibility logic here
+
+        // convert each user's results into a simple array of numbers
+        // comare the difference between current user's score against those from other users,
+        // ...question by question. Add up the differences to calculate the totalDifference
+        var totalDifference = 0;
+
+        // once you've found the current user's most comatible friend, display the result
+        // ...as a modal pop-up. THe modal should display both the name and picture of the closest match.
+        
     });
 }
--- server.js
+++ server.js
@@ -19,10 +19,14 @@ app.use(bodyParser.json());
 
 // points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-// listener, starts our server
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
